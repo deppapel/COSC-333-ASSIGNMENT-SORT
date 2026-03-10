bbc8328fe9107fa2ca4e1acadeab842ce13cc6ac
deppapel <maxwellomondi420@gmail.com>
2026-03-10 17:00:54 +0300

    initial commit

---
+++ gnome.py
@@ -0,0 +1,17 @@
+def gnome_sort(arr):
+   
+    i = 0
+    n = len(arr)
+
+    while i < n:
+        if i == 0:
+            i+=1
+        if arr[i] >= arr[i-1]:
+            i+=1
+        else:
+            arr[i], arr[i-1] = arr[i-1], arr[i]       
+            i-= 1
+       
+lists = [5, 1, 4, 2, 3]
+gnome_sort(lists)
+print("sorted is :", lists)
