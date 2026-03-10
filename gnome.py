4f6755f099ab1e18c718324680dddfc2e61968b3
deppapel <maxwellomondi420@gmail.com>
2026-03-10 17:54:59 +0300

    adding swap and commp count

--- gnome.py
+++ gnome.py
@@ -1,18 +1,25 @@
 # desc
 def gnome_sort(arr):
-   
+    comp = 0
+    swaps = 0
     i = 0
     n = len(arr)
 
     while i < n:
         if i == 0:
             i+=1
+            comp += 1
         if arr[i] <= arr[i-1]:
             i+=1
         else:
-            arr[i], arr[i-1] = arr[i-1], arr[i]       
-            i-= 1
+            arr[i], arr[i-1] = arr[i-1], arr[i] 
+            swaps += 1      
+            i-= 1   
+    return comp, swaps
        
-lists = [5, 1, 4, 2, 3]
-gnome_sort(lists)
+lists = [15, 19, 43, 26, 37]
+comp, swaps =gnome_sort(lists)
+
 print("sorted is :", lists)
+print("comparisons are:", comp)
+print("swaps are :", swaps)
