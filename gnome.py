261f6ea85c1822b0bd4799718420634d0d07a73f
deppapel <maxwellomondi420@gmail.com>
2026-03-19 17:31:25 +0300

    preview of sort

--- gnome.py
+++ gnome.py
@@ -1,6 +1,6 @@
 import random
 
-def gnome_sort(arr):
+def gnome_sort(arr, verbose=False):
     comp = 0
     swaps = 0
     i = 0
@@ -17,14 +17,28 @@ def gnome_sort(arr):
             arr[i], arr[i-1] = arr[i-1], arr[i]
             swaps += 1
             i -= 1
+            if verbose:
+                print(f" Swap {swaps}: {arr}")
     return comp, swaps
 
 def random_list(size, min_val=0, max_val=10**6):
     return [random.randint(min_val, max_val) for _ in range(size)]
 
+print("Testing Gnome Sort with random lists:")
+example = [5, 3, 2, 4, 1] 
+print("Original:", example)
+comp, swaps = gnome_sort(example)
+print("Sorted:", example)
+print(f"Comparisons: {comp}, Swaps: {swaps}\n")
+
 sizes = [1, 2, 3, 4, 5, 10, 250, 999, 9999]
 print("Size\tComparisons\tSwaps")
 for size in sizes:
     arr = random_list(size)
+    original = arr.copy()
     comp, swaps = gnome_sort(arr)
-    print(f"{size}\t{comp}\t\t{swaps}")
+
+    orig_preview = str(original[:5])[:-1] + ",...]" if size > 5 else str(original)
+    sort_preview = str(arr[:5])[:-1] + ",...]" if size > 5 else str(arr)
+
+    print(f"{size}\t{orig_preview}\t{sort_preview}\t{comp}\t\t{swaps}")
